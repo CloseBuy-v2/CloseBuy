28c125317c67fb226a738fbe631828bbe2b5eb2f
Salabh <hbk.salabh@gmail.com>
2016-03-06 02:18:57 +0530

    Side Menu Icon and Share Page
    Tell me which type of icon looks better.
    Also added a cordova plugin for app sharing purposes. Check out the
    "Share" page. Found some codes for it and pasted it app.ts, does not
    work now. Will fix it.
    Also, login and signup page look fine now. Tell me what else to add to
    those pages. Love you. :*

--- app/app.ts
+++ app/app.ts
@@ -2,6 +2,7 @@ import {App, Platform, MenuController, IonicApp} from 'ionic-framework/ionic';
 import {TabsPage} from './pages/tabs/tabs';
 import {LoginPage} from './pages/login/login';
 import {SignupPage} from './pages/signup/signup';
+import {SharePage} from './pages/share/share';
 import {Type} from 'angular2/core';
 
 
@@ -13,12 +14,14 @@ export class MyApp {
   rootPage: Type = TabsPage;
   menuLoginPage: LoginPage;
   menuSignupPage: SignupPage;
+  menuSharePage: SharePage;
   constructor(public app: IonicApp, public menu: MenuController, platform: Platform) {
     platform.ready().then(() => {
       this.app = app;
       this.menu = menu;
       this.menuLoginPage = LoginPage;
       this.menuSignupPage = SignupPage;
+      this.menuSharePage = SharePage;
     });
   }
   openPage(page) {
@@ -26,4 +29,25 @@ export class MyApp {
     let nav = this.app.getComponent('nav');
     nav.setRoot(page);
   }
+
+
+  share(message, subject, file, link) {
+        this.platform.ready().then(() => {
+            if(window.plugins.socialsharing) {
+                window.plugins.socialsharing.share(message, subject, file, link);
+            }
+        });
+    }
+ 
+  shareViaTwitter(message, image, link) {
+        this.platform.ready().then(() => {
+            if(window.plugins.socialsharing) {
+                window.plugins.socialsharing.canShareVia("twitter", message, null, image, link, function(result) {
+                    window.plugins.socialsharing.shareViaTwitter(message, image, link);
+                }, function(error) {
+                    console.error(error);
+                });
+            }
+        });
+    }
 }
---
+++ app/pages/share/share.ts
@@ -0,0 +1,7 @@
+import {Page} from 'ionic-framework/ionic';
+
+
+@Page({
+  templateUrl: 'build/pages/share/share.html',
+})
+export class SharePage {}
