9d4ea29bd1cc7513eabb5f9066b53cb32672a400
Salabh <hbk.salabh@gmail.com>
2016-03-08 02:21:59 +0530

    Share page not working.

--- app/pages/share/share.ts
+++ app/pages/share/share.ts
@@ -1,7 +1,31 @@
-import {Page} from 'ionic-angular';
+import {Page, Platform} from 'ionic-angular';
 
 
 @Page({
   templateUrl: 'build/pages/share/share.html',
 })
-export class SharePage {}
+export class SharePage {
+	constructor(platform: Platform) {
+        this.platform = platform;
+    }
+ 
+    share(message, subject, file, link) {
+        this.platform.ready().then(() => {
+            if(window.plugins.socialsharing) {
+                window.plugins.socialsharing.share(message, subject, file, link);
+            }
+        });
+    }
+ 
+    shareViaTwitter(message, image, link) {
+        this.platform.ready().then(() => {
+            if(window.plugins.socialsharing) {
+                window.plugins.socialsharing.canShareVia("twitter", message, null, image, link, function(result) {
+                    window.plugins.socialsharing.shareViaTwitter(message, image, link);
+                }, function(error) {
+                    console.error(error);
+                });
+            }
+        });
+    }
+}
