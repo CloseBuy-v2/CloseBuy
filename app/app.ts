c5f2a9be470b1b11c2105d9b10a3c9e3b9c61aa1
c3x04 <akashsharma95@outlook.com>
2016-03-29 00:06:47 +0530

    Remove dead code

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,5 @@
 import {App, IonicApp, Platform, MenuController} from 'ionic-angular';
 import {TabsPage} from './pages/tabs/tabs';
-import {LocationPage} from './pages/location/location';
 import {Type} from 'angular2/core';
 import {Offers} from './services/OffersService';
 import {OffersCategory} from './services/OffersCategories';
@@ -13,7 +12,6 @@ import {StatusBar} from 'ionic-native';
 })
 export class MyApp {
   rootPage: Type = TabsPage;
-  locationService: LocationPage;
   constructor(public app: IonicApp, public menu: MenuController, platform: Platform) {
     platform.ready().then(() => {
       this.app = app;
